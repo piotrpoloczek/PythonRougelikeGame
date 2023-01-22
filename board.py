e19db598ca7c53d505005eda168e8525de07c369
piotrpoloczek <piotr.jan.poloczek@gmail.com>
2023-01-22 14:30:41 +0100

    add board module

---
+++ board.py
@@ -0,0 +1,5 @@
+def get_board_height(board):
+    return len(board)
+
+def get_board_width(board):
+    return len(board[0])
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from main import DIRECTIONS, BOARD_HEIGHT, BOARD_WIDTH
+from board import get_board_height, get_board_width
 
 PLAYER_ICON = "@"
 PLAYER_START_X = 3
@@ -22,7 +23,7 @@ def create_player():
 
 def move(key, player, board):
     dx, dy = DIRECTIONS[key]
-    if 0 <= (player["x"] + dx ) < BOARD_WIDTH and 0 <= (player["y"] + dy) < BOARD_HEIGHT:
+    if 0 <= (player["x"] + dx ) < get_board_width(board) and 0 <= (player["y"] + dy) < get_board_height(board):
         player["x"] += dx
         player["y"] += dy
         board[player["y"]-dy][player["x"]-dx] = ' '
