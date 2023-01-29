f86f7e8cf15a95dbf02945a9a5c1cbf9f390a864
piotrpoloczek <piotr.jan.poloczek@gmail.com>
2023-01-29 15:02:03 +0100

    change map

--- game/game_turn.py
+++ game/game_turn.py
@@ -4,12 +4,14 @@ from board.board_view import display_board
 from coordinates.coordinates_function import input_to_directions
 from exception.exception_custom import CoordinateException
 from character.character_movement import try_move
+from view.view_functions import clear_screen
 
 
 def turn_run(game):
     player, level = get_game(game)
     board, opponents = get_board_and_opponents(level)
 
+    clear_screen()
     display_board(board)
 
     player_turn(player, board)
@@ -20,7 +22,7 @@ def player_turn(player, board):
     user_input = get_user_key()
     try:
         direction = input_to_directions(user_input)
-        print(player, direction, user_input)
+        #print(player, direction, user_input)
         try_move(player, direction, board)
     except CoordinateException:
         player_turn(player, board)
