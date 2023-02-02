8f69b7e1ac79f9d8fd878c02b31586140f60c389
Niram11 <dominikwidera2@gmail.com>
2023-02-02 14:35:15 +0100

    refaktoryzacja print_board, dodanie uniwersalnych funkcji

--- view/print_board.py
+++ view/print_board.py
@@ -1,7 +1,6 @@
 from view.menu_const import *
 
 def print_board(board,player):
-    
     board_m = create_menu(player)
     for idx,i in enumerate(board):
         line = ''
@@ -11,7 +10,6 @@ def print_board(board,player):
             else:
                 line += f" {j}"
         print(line)
-
 
 def create_menu(player):
     menu_board = create_menu_board()
@@ -25,54 +23,30 @@ def create_menu(player):
     menu_board = display_inventory(player,menu_board)
     return menu_board
 
-
 def create_menu_board():
     board = [[MENU_SPACER for j in range(MENU_WIDTH)] for i in range(MENU_HEIGHT)]
     return board
 
 def player_header_menu():
-    line = [MENU_SPACER]
-    line = input_data_to_line(line,MENU_PLAYER_HEADER)
-    line = fill_line(line)
-    return line
+    return fill_line(data=[MENU_PLAYER_HEADER])
 
 def player_name_menu(player):
-    line = [MENU_SPACER]
-    line = input_data_to_line(line,player['name'])
-    line = fill_line(line)
-    return line
+    return fill_line(data=[player['name']])
 
 def player_lvl_menu(player):
-    line = [MENU_SPACER]
-    line = input_data_to_line(line,MENU_LVL_HEADER)
-    line = fill_line(line)
-    return line
+    return fill_line(data=[MENU_LVL_HEADER])
 
 def player_experience_menu(player):
-    line = [MENU_SPACER]
-    line = input_data_to_line(line,MENU_EXPERIENCE_HEADER)
-    line = fill_line(line)
-    return line
+    return fill_line(data=[MENU_EXPERIENCE_HEADER])
 
 def player_hp_menu(player):
-    line = [MENU_SPACER]
-    line = input_data_to_line(line,MENU_HP_HEADER)
-    line = input_data_to_line(line,str(player['hp']))
-    line = fill_line(line)
-    return line
+    return fill_line(data=[MENU_HP_HEADER,str(player['hp'])])
 
 def player_attack_menu(player):
-    line = [MENU_SPACER]
-    line = input_data_to_line(line,MENU_ATTACK_HEADER)
-    line = input_data_to_line(line,str(player['attack']))
-    line = fill_line(line)
-    return line
+    return fill_line(data=[MENU_ATTACK_HEADER,str(player['attack'])])
 
 def player_inventory_menu():
-    line = [MENU_SPACER]
-    line = input_data_to_line(line,MENU_INVENTORY_HEADER)
-    line = fill_line(line)
-    return line
+    return fill_line(data=[MENU_INVENTORY_HEADER])
 
 def display_inventory(player,menu_board):
     inventory = player['inventory']
@@ -87,18 +61,21 @@ def display_inventory(player,menu_board):
     sorted_items = dict(items)
     keysList = [key for key in sorted_items]
     for idx in range(len(items)):
-        line = [MENU_SPACER]
-        line = input_data_to_line(line,keysList[idx])
-        line.append(':')
-        line = input_data_to_line(line,str(sorted_items_copy[idx][1]))
-        line = fill_line(line)
+        line = fill_line(data=[keysList[idx], ':', str(sorted_items_copy[idx][1])])
         menu_board[MENU_INVENTORY_POSITION+idx] = line
         if MENU_INVENTORY_POSITION + idx == MENU_HEIGHT - 1:
             return menu_board
     return menu_board
 
-def fill_line(line):
+def fill_end_of_line(line):
     return line + [ MENU_SPACER for i in range(MENU_WIDTH-len(line))]
 
 def input_data_to_line(line,data):
     return line + [i for i in data]
+
+def fill_line(data):
+    line = [MENU_SPACER]
+    for i in data:
+        line = input_data_to_line(line,i)
+    line = fill_end_of_line(line)
+    return line
